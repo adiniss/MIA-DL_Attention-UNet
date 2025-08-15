b3fcb82ad0d71f9f63c181d8a740dde855d4cec9
adiniss <155883802+adiniss@users.noreply.github.com>
2025-08-15 18:27:22 +0300

    Updated DiceCE computation (compound loss for binary image segmentation)

--- src/medattn/losses/dicece.py
+++ src/medattn/losses/dicece.py
@@ -1,14 +1,22 @@
 import torch, torch.nn as nn
 
 class DiceCE(nn.Module):
+    # Compound loss of Dice + CE for our binary image segmentation
     def __init__(self):
         super().__init__()
-        self.ce = nn.BCEWithLogitsLoss()
-    def forward(self, logits, y):
-        ce = self.ce(logits, y)
-        p = torch.sigmoid(logits)
+        self.LogitsCE = nn.BCEWithLogitsLoss()
+
+    def forward(self, logits, labels):
+
+        # Compute binary cross entropy (after sigmoid)
+        BCE = self.LogitsCE(logits, labels)
+
+        # Compute dice loss
+        prediction = torch.sigmoid(logits)
         eps = 1e-6
-        num = 2*(p*y).sum(dim=(2,3))
-        den = (p*p + y*y).sum(dim=(2,3)) + eps
-        dice = 1 - (num/den).mean()
-        return ce + dice
+        dice_num = 2 * (prediction * labels).sum(dim=(2, 3))
+        dice_den = (prediction * prediction + labels * labels).sum(dim=(2, 3)) + eps
+        dice = 1 - (dice_num / dice_den).mean()
+
+        compound_loss = BCE + dice
+        return compound_loss
